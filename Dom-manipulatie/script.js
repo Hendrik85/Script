b9d68a6e01503a54ce1516ac6ebc661fe5e717cc
Hendrik85 <hhc.beukema@gmail.com>
2024-09-10 14:16:33 +0200

    first commit

---
+++ Dom-manipulatie/script.js
@@ -0,0 +1,15 @@
+const btnToggleName = document.getElementById("btnToggleName");
+const nameContainer = document.getElementById("nameContainer");
+const myName = "Hendrik";
+btnToggleName.addEventListener("click", toggleName);
+
+function toggleName() {
+    nameContainer.innerHTML = "Hi Hendrik";
+}
+
+const colors = ['red', 'red', 'green', 'yellow', 'orange'];
+colors.forEach(function(number)
+document.getElementById("nameContainer").innerHTML = Math.random();
+
+
+
